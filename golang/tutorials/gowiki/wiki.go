e24cff56b5c95fb0960866f581aea810ec9b7515
Sebastian Gorecki <sebastian.gorecki@olx.pl>
2021-09-30 22:09:06 +0200

    Go Wiki project: Adds Template caching

--- golang/tutorials/gowiki/wiki.go
+++ golang/tutorials/gowiki/wiki.go
@@ -12,6 +12,8 @@ type Page struct {
 	Body  []byte
 }
 
+var templates = template.Must(template.ParseFiles("view.html", "edit.html"))
+
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
@@ -27,14 +29,10 @@ func loadPage(title string) (*Page, error) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, err := template.ParseFiles(tmpl + ".html")
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
-	}
-	err = t.Execute(w, p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
