7d27ea908139fb87e64b8eee577bb40a361a0c3d
Sebastian Górecki <sebastian.gorecki@olx.pl>
2022-02-19 22:02:38 +0100

    gdoc-writer: Updates document style. Inserts first text into doc

--- golang/gdoc-writer/gdoc-writer.go
+++ golang/gdoc-writer/gdoc-writer.go
@@ -95,4 +95,21 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to create document: %v", err)
 	}
+
+	md := docs.Dimension{Magnitude: 20.0, Unit: "PT"}
+	style := docs.DocumentStyle{MarginLeft: &md, MarginRight: &md, MarginTop: &md}
+	styleRequest := docs.UpdateDocumentStyleRequest{DocumentStyle: &style, Fields: "marginTop,marginLeft,marginRight"}
+
+	ins1 := docs.InsertTextRequest{Text: "Second line", Location: &docs.Location{Index: 1}}
+	ins2 := docs.InsertTextRequest{Text: "Seba was here!\n", Location: &docs.Location{Index: 1}}
+
+	requests := make([]*docs.Request, 0)
+	requests = append(requests, &docs.Request{UpdateDocumentStyle: &styleRequest})
+	requests = append(requests, &docs.Request{InsertText: &ins1})
+	requests = append(requests, &docs.Request{InsertText: &ins2})
+	_, err = srv.Documents.BatchUpdate(doc.DocumentId, &docs.BatchUpdateDocumentRequest{Requests: requests}).Do()
+	if err != nil {
+		log.Fatalf("Update to update document stype: %v", err)
+	}
+
 }
