ae52dfdfadf846d17035b5c4fe897913b2604068
Sebastian Górecki <sebastian.gorecki@olx.pl>
2022-07-13 20:16:33 +0200

    terraform: sample github repo

---
+++ terraform/beginner-to-advanced-course/github.tf
@@ -0,0 +1,17 @@
+# terraform {
+#  required_providers {
+#    github = {
+#      source = "integrations/github"
+#      version = "4.26.1"
+#    }
+#  }
+#}
+
+#provider "github" {
+#  token = ""
+#}
+
+#resource "github_repository" "example" {
+#  name = "terraform-repo"
+#  visibility = "private"
+#}
