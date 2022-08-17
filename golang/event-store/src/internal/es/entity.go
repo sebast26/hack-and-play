adae1d73a9311098763fbd665036fa6e03d02d0f
Sebastian Górecki <sebastian.gorecki@olx.pl>
2022-08-17 12:12:21 +0200

    event-store: before changes to entities; only base interfaces and entities added.

--- golang/event-store/src/internal/es/entity.go
+++ golang/event-store/src/internal/es/entity.go
@@ -1,16 +1,19 @@
 package es
 
+// EventSourcer interface for event-based entities (following Event Sourcing pattern).
 type EventSourcer interface {
 	Apply(EventSourcer, interface{})
 	When()
 }
 
+// Entity is a base type for all event-based entities.
 type Entity struct {
 	ID      string
 	Version int
 	Changes []interface{}
 }
 
+// Apply generic method that applies event on given entity.
 func (e *Entity) Apply(entity EventSourcer, event interface{}) {
 	entity.When()
 	e.Changes = append(e.Changes, event)
