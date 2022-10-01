76c5dab08fffcba396b4d448cb4b4675a760f8c5
Sebastian Górecki <sebastian.gorecki@olx.pl>
2022-10-01 21:19:03 +0200

    Terraform course: Data sources

---
+++ terraform/beginner-to-advanced-course/data-sources/datasource.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    region = "eu-west-1"
+}
+
+data "aws_ami" "app_ami" {
+    most_recent = true
+    owners = ["amazon"]
+
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm*"]
+    }
+}
+
+resource "aws_instance" "ec2-dev" {
+    ami = data.aws_ami.app_ami.id
+    instance_type = "t2.micro"
+}
