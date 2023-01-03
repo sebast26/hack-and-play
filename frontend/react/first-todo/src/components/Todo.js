6f4b0f9b28382d34df0f54ac8dcefd828c2218ee
Sebastian Górecki <sebastian.gorecki@olx.pl>
2023-01-03 16:33:29 +0100

    events handler

--- frontend/react/first-todo/src/components/Todo.js
+++ frontend/react/first-todo/src/components/Todo.js
@@ -1,10 +1,12 @@
 // has to start with capital 'T'
 function Todo(props) {
+  function deleteHandler() {}
+
   return (
     <div className={'card'}>
       <h2>{props.text}</h2>
       <div className={'actions'}>
-        <button className={'btn'}>Delete</button>
+        <button className={'btn'} onClick={deleteHandler}>Delete</button>
       </div>
     </div>
   )
