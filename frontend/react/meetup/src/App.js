2f041ba7a30819350a01479465475830c6901d75
Sebastian Górecki <sebastian.gorecki@olx.pl>
2023-01-03 22:59:05 +0100

    meetup: navigation

--- frontend/react/meetup/src/App.js
+++ frontend/react/meetup/src/App.js
@@ -2,10 +2,12 @@ import {Route, Routes} from 'react-router-dom'
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import Favorites from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation/>
       <Routes>
         <Route path={'/'} element={<AllMeetupsPage/>}/>
         <Route path={'/new-meetup'} element={<NewMeetupPage/>}/>
