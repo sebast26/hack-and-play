00d4b1ebb003b65c8a2cd1f0ca1f02fc0ed8184f
Sebastian Górecki <sebastian.gorecki@olx.pl>
2023-01-17 23:28:08 +0100

    meetup: favorites page and my favorites counter

--- frontend/react/meetup/src/components/layout/MainNavigation.js
+++ frontend/react/meetup/src/components/layout/MainNavigation.js
@@ -1,7 +1,10 @@
 import {Link} from 'react-router-dom'
 import classes from './MainNavigation.module.css'
+import {useContext} from "react";
+import FavoritesContext from "../../store/favorites-context";
 
 function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext)
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -9,7 +12,10 @@ function MainNavigation() {
         <ul>
           <li><Link to={'/'}>All Meetups</Link></li>
           <li><Link to={'/new-meetup'}>Add New Meetups</Link></li>
-          <li><Link to={'/favorites'}>My Favorites</Link></li>
+          <li><Link to={'/favorites'}>
+            My Favorites
+            <span className={classes.badge}>{favoritesCtx.totalFavorites}</span>
+          </Link></li>
         </ul>
       </nav>
     </header>
