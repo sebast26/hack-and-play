bbb09c795836e93a32f8a4e1e8f8f7a8a368189b
Sebastian Górecki <sebastian.gorecki@olx.pl>
2023-03-06 20:43:01 +0100

    refactoring to kotlin: Cleaning up

--- kotlin/gildedrose-refactoring/src/main/java/com/gildedrose/Item.kt
+++ kotlin/gildedrose-refactoring/src/main/java/com/gildedrose/Item.kt
@@ -8,77 +8,10 @@ class Item(
     private val degradation: (Int, Int) -> Int = Degradation.standard,
     private val saturation: (Int) -> Int = Saturation.standard
 ) {
-
-    override fun toString(): String = "$name, $sellIn, $quality"
-    
     fun update() {
         sellIn = sellIn - aging()
         quality = saturation(quality - degradation(sellIn, quality))
     }
+
+    override fun toString(): String = "$name, $sellIn, $quality"
 }
-
-object Aging {
-    val standard: () -> Int = { 1 }
-    val none: () -> Int = { 0 }
-}
-
-object Degradation {
-    val standard: (Int, Int) -> Int = { sellIn: Int, quality: Int ->
-        when {
-            sellIn < 0 -> 2
-            else -> 1
-        }
-    }
-    val none: (Int, Int) -> Int = { _, _ -> 0 }
-}
-
-object Saturation {
-    val standard: (Int) -> Int = { quality: Int ->
-        when {
-            quality < 0 -> 0
-            quality > 50 -> 50
-            else -> quality
-        }
-    }
-    val none: (Int) -> Int = { quality -> quality }
-}
-
-fun Sulfuras(name: String, sellIn: Int, quality: Int) = Item(
-    name,
-    sellIn,
-    quality,
-    aging = Aging.none,
-    degradation = Degradation.none,
-    saturation = Saturation.none
-)
-
-fun Brie(name: String, sellIn: Int, quality: Int) = Item(
-    name,
-    sellIn,
-    quality,
-    degradation = Degradation.standard * -1
-)
-
-fun Pass(name: String, sellIn: Int, quality: Int) = Item(
-    name,
-    sellIn,
-    quality,
-    degradation = fun(currentSellIn: Int, currentQuality: Int): Int = when {
-        currentSellIn < 0 -> currentQuality
-        currentSellIn < 5 -> -3
-        currentSellIn < 10 -> -2
-        else -> -1
-    }
-)
-
-fun Conjured(name: String, sellIn: Int, quality: Int) = Item(
-    name,
-    sellIn,
-    quality,
-    degradation = Degradation.standard * 2
-)
-
-operator fun ((Int, Int) -> Int).times(multiplier: Int) = { p1: Int, p2: Int ->
-    this(p1, p2) * multiplier
-}
-
---
+++ kotlin/gildedrose-refactoring/src/main/java/com/gildedrose/items.kt
@@ -0,0 +1,36 @@
+package com.gildedrose
+
+fun Sulfuras(name: String, sellIn: Int, quality: Int) = Item(
+    name,
+    sellIn,
+    quality,
+    aging = Aging.none,
+    degradation = Degradation.none,
+    saturation = Saturation.none
+)
+
+fun Brie(name: String, sellIn: Int, quality: Int) = Item(
+    name,
+    sellIn,
+    quality,
+    degradation = Degradation.standard * -1
+)
+
+fun Pass(name: String, sellIn: Int, quality: Int) = Item(
+    name,
+    sellIn,
+    quality,
+    degradation = fun(currentSellIn: Int, currentQuality: Int): Int = when {
+        currentSellIn < 0 -> currentQuality
+        currentSellIn < 5 -> -3
+        currentSellIn < 10 -> -2
+        else -> -1
+    }
+)
+
+fun Conjured(name: String, sellIn: Int, quality: Int) = Item(
+    name,
+    sellIn,
+    quality,
+    degradation = Degradation.standard * 2
+)
