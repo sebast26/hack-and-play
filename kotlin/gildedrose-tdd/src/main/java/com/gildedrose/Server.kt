f112ef254ecf4eb1e6e8a43aef6b6220242ed9be
Sebastian Górecki <sebastian.gorecki@olx.pl>
2023-03-13 23:03:37 +0100

    gildedrose-tdd: Better table formatting

--- kotlin/gildedrose-tdd/src/main/java/com/gildedrose/Server.kt
+++ kotlin/gildedrose-tdd/src/main/java/com/gildedrose/Server.kt
@@ -1,8 +1,5 @@
 package com.gildedrose
 
-import com.github.jknack.handlebars.Handlebars
-import com.github.jknack.handlebars.Template
-import com.github.jknack.handlebars.io.StringTemplateSource
 import org.http4k.core.Method
 import org.http4k.core.Response
 import org.http4k.core.Status
@@ -10,6 +7,8 @@ import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.server.Undertow
 import org.http4k.server.asServer
+import org.http4k.template.HandlebarsTemplates
+import org.http4k.template.ViewModel
 import org.intellij.lang.annotations.Language
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
@@ -24,25 +23,27 @@ class Server(
     val routes = routes(
         "/" bind Method.GET to { _ ->
             val now = clock()
-            Response(Status.OK).body(rootTemplate.apply(
-                mapOf(
-                    "now" to dateFormat.format(now),
-                    "items" to stock.map { it.toMap(now) }
+            Response(Status.OK).body(handlebars(
+                StockListViewModel(
+                    now = dateFormat.format(now),
+                    items = stock.map { it.toMap(now) }
                 )
             ))
         }
     )
 
-    private val handlebars = Handlebars()
-    private val rootTemplate: Template = handlebars.compile(
-        StringTemplateSource("no such file", templateSource)
-    )
+    private val handlebars = HandlebarsTemplates().HotReload("src/main/java")
 
     fun start() {
         val http4kServer = routes.asServer(Undertow(8080))
         http4kServer.start()
     }
 }
+
+private data class StockListViewModel(
+    val now: String,
+    val items: List<Map<String, String>>
+) : ViewModel
 
 private fun Item.toMap(now: LocalDate): Map<String, String> = mapOf(
     "name" to name,
