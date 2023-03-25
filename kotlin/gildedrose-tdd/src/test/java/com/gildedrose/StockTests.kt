1ad50b712dd2eac3e89e2c333b235debc73f79ea
Sebastian Górecki <sebastian.gorecki@olx.pl>
2023-03-25 20:11:40 +0100

    gildedrose-tdd: Instroduce Instant.daysTo

--- kotlin/gildedrose-tdd/src/test/java/com/gildedrose/StockTests.kt
+++ kotlin/gildedrose-tdd/src/test/java/com/gildedrose/StockTests.kt
@@ -4,24 +4,26 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.io.File
 import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
 
 
 class StockTests {
     private val initialStockList = standardStockList.copy(
-        lastModified = Instant.parse("2023-03-25T23:59:59Z")
+        lastModified = Instant.parse("2023-03-24T23:59:59Z")
     )
     private val fixture = Fixture(initialStockList)
-    private val stock = Stock(fixture.stockFile)
+    private val stock = Stock(fixture.stockFile, ZoneId.of("Europe/London"))
 
     @Test
     fun `loads stock from file`() {
-        val now = Instant.parse("2023-03-25T23:59:59Z")
+        val now = Instant.parse("2023-03-24T23:59:59Z")
         assertEquals(initialStockList, stock.stockList(now))
     }
 
     @Test
     fun `updates stock if last modified yesterday`() {
-        val now = Instant.parse("2023-03-26T00:00:01Z")
+        val now = Instant.parse("2023-03-25T00:00:01Z")
         val expectedUpdateResult = initialStockList.copy(lastModified = now)
         assertEquals(expectedUpdateResult, stock.stockList(now))
         assertEquals(expectedUpdateResult, fixture.load())
@@ -29,11 +31,12 @@ class StockTests {
 }
 
 class Stock(
-    val stockFile: File
+    private val stockFile: File,
+    private val zoneID: ZoneId
 ) {
     fun stockList(now: Instant): StockList {
         val loaded = stockFile.loadItems()
-        return if (loaded.lastModified.daysBetween(now) == 0L) {
+        return if (loaded.lastModified.daysTo(now, zoneID) == 0L) {
             loaded
         } else {
             loaded.copy(lastModified = now).also {
@@ -43,8 +46,5 @@ class Stock(
     }
 }
 
-private fun Instant.daysBetween(then: Instant): Long = when (then) {
-    Instant.parse("2023-03-26T00:00:01Z") -> 1
-    Instant.parse("2023-03-25T23:59:59Z") -> 0
-    else -> TODO()
-}
+internal fun Instant.daysTo(that: Instant, zone: ZoneId): Long =
+    LocalDate.ofInstant(that, zone).toEpochDay() - LocalDate.ofInstant(this, zone).toEpochDay()
