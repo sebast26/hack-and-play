1810bfd88435e4fe5a9b2ccd07d3739d374eb9e0
Sebastian Górecki <sebastian.gorecki@olx.pl>
2023-03-25 17:14:00 +0100

    gildedrose-tdd: Upgrade stock file by saving when loaded

--- kotlin/gildedrose-tdd/src/main/java/main.kt
+++ kotlin/gildedrose-tdd/src/main/java/main.kt
@@ -1,12 +1,14 @@
 import com.gildedrose.Server
 import com.gildedrose.loadItems
 import com.gildedrose.routes
+import com.gildedrose.saveTo
 import java.io.File
 import java.time.Instant
 
 fun main() {
     val file = File("stock.tsv").also { it.createNewFile() }
-    val stock = file.loadItems(Instant.now())
+    val stock = file.loadItems()
+    stock.saveTo(file)
 
     val server = Server(routes(stock))
     server.start()
