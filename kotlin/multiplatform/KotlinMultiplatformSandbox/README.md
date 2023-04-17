a326811f26f2bd55c39deff8f82f5bed071b6831
Sebastian Górecki <sebastian.gorecki@olx.pl>
2023-04-17 17:42:53 +0200

    kotlin-multiplatform: complete 'Get started with Kotlin Multiplatform for mobile'

---
+++ kotlin/multiplatform/KotlinMultiplatformSandbox/README.md
@@ -0,0 +1,3 @@
+# KotlinMultiplatform 
+
+Based on "Get started with Kotlin Multiplatform for mobile" from https://kotlinlang.org/docs/multiplatform-mobile-getting-started.html
---
+++ kotlin/multiplatform/KotlinMultiplatformSandbox/androidApp/src/main/java/me/sgorecki/kotlinmultiplatformsandbox/android/MainActivity.kt
@@ -0,0 +1,48 @@
+package me.sgorecki.kotlinmultiplatformsandbox.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import me.sgorecki.kotlinmultiplatformsandbox.Greeting
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    var text by remember { mutableStateOf("Loading") }
+                    LaunchedEffect(true) {
+                        text = try {
+                            Greeting().greet()
+                        } catch (e: Exception) {
+                            e.localizedMessage ?: "error"
+                        }
+                    }
+                    GreetingView(text)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingView(text: String) {
+    Text(text = text)
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+        GreetingView("Hello, Android!")
+    }
+}
