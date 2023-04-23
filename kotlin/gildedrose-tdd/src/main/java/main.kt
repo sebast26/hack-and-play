5316e99c39ae34bef8f53b31f623f33af948d952
Sebastian Górecki <sebastian.gorecki@olx.pl>
2023-04-23 13:41:46 +0200

    gildedrose-tdd: Install CatchAll filter in case of errors

--- kotlin/gildedrose-tdd/src/main/java/main.kt
+++ kotlin/gildedrose-tdd/src/main/java/main.kt
@@ -4,8 +4,14 @@ import com.gildedrose.listHandler
 import com.gildedrose.updateItems
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.core.then
+import org.http4k.filter.ServerFilters
 import org.http4k.routing.bind
 import org.http4k.routing.routes
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.io.File
 import java.time.Instant
 import java.time.ZoneId
@@ -23,7 +29,15 @@ fun routesFor(
     clock: () -> Instant
 ): HttpHandler {
     val stock = Stock(stockFile, londonZoneId, ::updateItems)
-    return routes(
-        "/" bind GET to listHandler(clock, londonZoneId, stock::stockList)
-    )
+    return catchAllFilter.then(
+        routes(
+            "/" bind GET to listHandler(clock, londonZoneId, stock::stockList),
+            "/errors" bind GET to { error("deliberate") }
+        ))
+}
+
+val logger: Logger = LoggerFactory.getLogger("Uncaught Exceptions")
+val catchAllFilter = ServerFilters.CatchAll {
+    logger.error("Uncaught exception", it)
+    Response(Status.INTERNAL_SERVER_ERROR).body("Something went wrong, sorry")
 }
