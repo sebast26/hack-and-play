4b649fcc4acfd8931ab1a17543feae4aa7d41191
Sebastian Górecki <sebastian.gorecki@olx.pl>
2023-04-25 21:29:39 +0200

    Revert "gildedrose-tdd: Getting Better with Age: Aged Brie"
    This reverts commit 075fe162ff2e4723e00993ceeb20361d9091daa7.

--- kotlin/gildedrose-tdd/src/main/java/com/gildedrose/Item.kt
+++ kotlin/gildedrose-tdd/src/main/java/com/gildedrose/Item.kt
@@ -7,10 +7,6 @@ data class Item(
     val sellByDate: LocalDate?,
     val quality: Int
 ) {
-    private val updater: (on: LocalDate) -> Item =
-        if (this.name == "Aged Brie") this::updateBrie
-        else this::updateStandard
-
     init {
         require(quality >= 0) {
             "Quality is $quality but should not be negative"
@@ -19,24 +15,15 @@ data class Item(
 
     fun updatedBy(days: Int, on: LocalDate): Item {
         val dates = (1 - days).rangeTo(0).map { on.plusDays(it.toLong()) }
-        return dates.fold(this) { item, date -> item.updater(date) }
+        return dates.fold(this) { item, date -> item.update(date) }
     }
-}
 
-private fun Item.updateStandard(on: LocalDate): Item {
-    val degradation = when {
-        sellByDate == null -> 0
-        on.isAfter(sellByDate) -> 2
-        else -> 1
+    private fun update(on: LocalDate): Item {
+        val degradation = when {
+            sellByDate == null -> 0
+            on.isAfter(sellByDate) -> 2
+            else -> 1
+        }
+        return copy(quality = (quality - degradation).coerceAtLeast(0))
     }
-    return copy(quality = (quality - degradation).coerceAtLeast(0))
-}
-
-private fun Item.updateBrie(on: LocalDate): Item {
-    val improvement = when {
-        sellByDate == null -> 0
-        on.isAfter(sellByDate) -> 2
-        else -> 1
-    }
-    return copy(quality = (quality + improvement).coerceAtMost(50))
 }
