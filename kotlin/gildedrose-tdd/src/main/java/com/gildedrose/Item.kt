083e87f10466278bf8bfba06fdaa9f1a7ab2712f
Sebastian Górecki <sebastian.gorecki@olx.pl>
2023-04-25 22:44:23 +0200

    gildedrose-tdd: Duncan: replace with Item as base class';

--- kotlin/gildedrose-tdd/src/main/java/com/gildedrose/Item.kt
+++ kotlin/gildedrose-tdd/src/main/java/com/gildedrose/Item.kt
@@ -2,15 +2,37 @@ package com.gildedrose
 
 import java.time.LocalDate
 
-abstract class Item {
-    abstract val name: String
-    abstract val quality: Int
-    abstract fun updated(on: LocalDate): Item
-    abstract fun withQuality(quality: Int): Item
+open class Item(
+    val name: String,
+    val quality: Int
+) {
+    init {
+        require(quality >= 0) {
+            "Quality is $quality but should not be negative"
+        }
+    }
+
+    open fun updated(on: LocalDate) = this
+    open fun withQuality(quality: Int) = Item(name, quality)
 
     fun updatedBy(days: Int, on: LocalDate): Item {
         val dates = (1 - days).rangeTo(0).map { on.plusDays(it.toLong()) }
         return dates.fold(this) { item, date -> item.updated(on = date) }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Item
+
+        if (name != other.name) return false
+        return quality == other.quality
+    }
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + quality
+        return result
     }
 }
 
@@ -19,36 +41,15 @@ fun Item(
     sellByDate: LocalDate?,
     quality: Int
 ) = when (sellByDate) {
-    null -> UndatedItem(name, quality)
+    null -> Item(name, quality)
     else -> DatedItem(name, sellByDate, quality)
 }
 
-data class UndatedItem(
-    override val name: String,
-    override val quality: Int
-) : Item() {
-    init {
-        require(quality >= 0) {
-            "Quality is $quality but should not be negative"
-        }
-    }
-
-    override fun updated(on: LocalDate) = this
-
-    override fun withQuality(quality: Int) = this.copy(quality = quality)
-}
-
-data class DatedItem(
-    override val name: String,
+class DatedItem(
+    name: String,
     val sellByDate: LocalDate,
-    override val quality: Int
-) : Item() {
-    init {
-        require(quality >= 0) {
-            "Quality is $quality but should not be negative"
-        }
-    }
-
+    quality: Int
+) : Item(name, quality) {
     override fun updated(on: LocalDate): DatedItem {
         val degradation = when {
             on.isAfter(sellByDate) -> 2
@@ -57,5 +58,19 @@ data class DatedItem(
         return withQuality(quality = (quality - degradation).coerceAtLeast(0))
     }
 
-    override fun withQuality(quality: Int) = this.copy(quality = quality)
+    override fun withQuality(quality: Int) = DatedItem(name, sellByDate, quality)
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        if (!super.equals(other)) return false
+
+        other as DatedItem
+
+        return sellByDate == other.sellByDate
+    }
+    override fun hashCode(): Int {
+        var result = super.hashCode()
+        result = 31 * result + sellByDate.hashCode()
+        return result
+    }
 }
