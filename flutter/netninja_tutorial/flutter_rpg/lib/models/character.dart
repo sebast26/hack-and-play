535d76b1dd183003bd2178802b729393283e48f2
Sebastian Górecki <gorecki.sebastian@gmail.com>
2024-04-16 22:08:02 +0200

    Flutter: character skills and vocations/classes

--- flutter/netninja_tutorial/flutter_rpg/lib/models/character.dart
+++ flutter/netninja_tutorial/flutter_rpg/lib/models/character.dart
@@ -1,11 +1,15 @@
+import 'package:flutter_rpg/models/skill.dart';
 import 'package:flutter_rpg/models/stats.dart';
+import 'package:flutter_rpg/models/vocation.dart';
 
 class Character with Stats{
 
   Character({
-    required this.name, required this.slogan, required this.id
+    required this.name, required this.slogan, required this.vocation, required this.id
   });
 
+  final Set<Skill> skills = {};
+  final Vocation vocation;
   final String name;
   final String slogan;
   final String id;
@@ -16,5 +20,10 @@ class Character with Stats{
 
   void toggleIsFav() {
     _isFav = !_isFav;
+  }
+
+  void updateSkill(Skill skill) {
+    skills.clear();
+    skills.add(skill);
   }
 }
