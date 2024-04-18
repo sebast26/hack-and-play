da11725ff5180ffb120a79378f66a1e177071638
Sebastian Górecki <gorecki.sebastian@gmail.com>
2024-04-18 11:07:23 +0200

    Flutter: adds input fields

--- flutter/netninja_tutorial/flutter_rpg/lib/main.dart
+++ flutter/netninja_tutorial/flutter_rpg/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/screens/create/create.dart';
 import 'package:flutter_rpg/screens/home/home.dart';
 import 'package:flutter_rpg/theme.dart';
 
 void main() {
   runApp(MaterialApp(
     theme: primaryTheme,
-    home: const Home(),
+    home: const Create(),
   ));
 }
 
