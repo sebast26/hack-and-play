1d2c277e8ad381bc427026f424e4c2867b1705b5
Sebastian Górecki <gorecki.sebastian@gmail.com>
2024-04-18 15:54:30 +0200

    Flutter: adding character to characters list

--- flutter/netninja_tutorial/flutter_rpg/lib/screens/create/create.dart
+++ flutter/netninja_tutorial/flutter_rpg/lib/screens/create/create.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/character.dart';
 import 'package:flutter_rpg/models/vocation.dart';
 import 'package:flutter_rpg/screens/create/vocation_card.dart';
 import 'package:flutter_rpg/shared/styled_button.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:uuid/uuid.dart';
+
+var uuid = const Uuid();
 
 class Create extends StatefulWidget {
   const Create({super.key});
@@ -37,15 +41,19 @@ class _CreateState extends State<Create> {
   //submit handler
   void handleSubmit() {
     if (_nameController.text.trim().isEmpty) {
-      print('Name must not be empty');
       return;
     }
     if (_sloganController.text.trim().isEmpty) {
-      print('Slogan must not be empty');
       return;
     }
-    print(_nameController.text);
-    print(_sloganController.text);
+    characters.add(
+      Character(
+        name: _nameController.text.trim(), 
+        slogan: _sloganController.text.trim(), 
+        vocation: selectedVocation, 
+        id: uuid.v4()
+      )
+    );
   }
 
   @override
@@ -132,6 +140,18 @@ class _CreateState extends State<Create> {
                 selected: selectedVocation == Vocation.wizard,
               ),
           
+              // good luck message
+              Center(
+                child: Icon(Icons.code, color: AppColors.primaryColor,),
+              ),
+              const Center(
+                child: StyledHeading('Good luck.'),
+              ),
+              const Center(
+                child: StyledText('And enjoy the journey...'),
+              ),
+              const SizedBox(height: 30,),
+
               Center(
                 child: StyledButton(
                   onPressed: handleSubmit,
