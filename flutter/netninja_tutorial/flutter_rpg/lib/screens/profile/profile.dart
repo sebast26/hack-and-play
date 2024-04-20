69490e4422d2a1c8d1ab9dec767e81f07dc918fd
Sebastian Górecki <gorecki.sebastian@gmail.com>
2024-04-20 20:14:44 +0200

    Flutter: adds skill list and toast message

--- flutter/netninja_tutorial/flutter_rpg/lib/screens/profile/profile.dart
+++ flutter/netninja_tutorial/flutter_rpg/lib/screens/profile/profile.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rpg/models/character.dart';
+import 'package:flutter_rpg/screens/profile/skills_list.dart';
 import 'package:flutter_rpg/screens/profile/stats_table.dart';
+import 'package:flutter_rpg/shared/styled_button.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
 
@@ -82,12 +84,25 @@ class Profile extends StatelessWidget {
               alignment: Alignment.center,
               child: Column(
                 children: [
-                  StatsTable(character)
+                  StatsTable(character),
+                  SkillList(character),
                 ],
               )
-            )
+            ),
 
             // save button
+            StyledButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: const StyledHeading('Character was saved.'),
+                  showCloseIcon: true,
+                  duration: const Duration(seconds: 2),
+                  backgroundColor: AppColors.secondaryColor,
+                ));
+              },
+              child: const StyledHeading('Save character'),
+            ),
+            const SizedBox(height: 20,),
           ],
         ),
       ),
