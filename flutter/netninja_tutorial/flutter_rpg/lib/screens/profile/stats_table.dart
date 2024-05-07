16b50c9aa99d8888adde435249740a39dfa15552
Sebastian Górecki <gorecki.sebastian@gmail.com>
2024-05-07 21:56:55 +0200

    Flutter: animated rotation

--- flutter/netninja_tutorial/flutter_rpg/lib/screens/profile/stats_table.dart
+++ flutter/netninja_tutorial/flutter_rpg/lib/screens/profile/stats_table.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_rpg/models/character.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
@@ -13,6 +14,8 @@ class StatsTable extends StatefulWidget {
 }
 
 class _StatsTableState extends State<StatsTable> {
+  double turns = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -25,8 +28,12 @@ class _StatsTableState extends State<StatsTable> {
             padding: const EdgeInsets.all(8),
             child: Row(
               children: [
-                Icon(Icons.star,
-                  color: widget.character.points > 0 ? Colors.yellow : Colors.grey,
+                AnimatedRotation(
+                  turns: turns,
+                  duration: const Duration(milliseconds: 200),
+                  child: Icon(Icons.star,
+                    color: widget.character.points > 0 ? Colors.yellow : Colors.grey,
+                  ),
                 ),
                 const SizedBox(width: 20,),
                 const StyledText('Stat points available: '),
@@ -65,6 +72,7 @@ class _StatsTableState extends State<StatsTable> {
                       onPressed: () {
                         setState(() {
                           widget.character.increaseStat(stat['title']!);  
+                          turns += 0.5;
                         });
                       },
                     ),
@@ -76,6 +84,7 @@ class _StatsTableState extends State<StatsTable> {
                       onPressed: () {
                         setState(() {
                           widget.character.decreaseStat(stat['title']!);  
+                          turns -= 0.5;
                         });
                       },
                     ),
