e3852b6801d0482f30f97a5a2e85673acd75cf54
Sebastian Górecki <gorecki.sebastian@gmail.com>
2024-08-27 21:49:29 +0200

    gradle: configure gradle, modify compile java task, adds taskinfo plugin

--- gradle/dpeuniversity/lab/app/build.gradle.kts
+++ gradle/dpeuniversity/lab/app/build.gradle.kts
@@ -8,6 +8,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    id("org.barfuin.gradle.taskinfo") version "2.2.0"
 }
 
 repositories {
@@ -40,4 +41,8 @@ application {
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
+}
+
+tasks.named<JavaCompile>("compileJava") {
+    this.options.isDebug = false
 }
