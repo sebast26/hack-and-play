89dc8c7efa72697ba9d095cdc88826e65f21f993
Sebastian Górecki <gorecki.sebastian@gmail.com>
2025-04-23 21:46:34 +0200

    Duncan GildedRose Refactoring: adds age and update3 methods

--- kotlin/duncan-gildedrose-refactoring/src/main/java/com/gildedrose/Item.kt
+++ kotlin/duncan-gildedrose-refactoring/src/main/java/com/gildedrose/Item.kt
@@ -11,33 +11,25 @@ open class Item(
 open class BaseItem(name: String, sellIn: Int, quality: Int) : Item(name, sellIn, quality) {
     fun update() {
         update1()
-
-        if (name != "Sulfuras, Hand of Ragnaros") {
-            sellIn = sellIn - 1
-        }
-
-        if (sellIn < 0) {
-            if (name != "Aged Brie") {
-                if (name != "Backstage passes to a TAFKAL80ETC concert") {
-                    if (quality > 0) {
-                        if (name != "Sulfuras, Hand of Ragnaros") {
-                            quality = quality - 1
-                        }
-                    }
-                } else {
-                    quality = quality - quality
-                }
-            } else {
-                if (quality < 50) {
-                    quality = quality + 1
-                }
-            }
-        }
+        age()
+        update3()
     }
 
     protected open fun update1() {
         if (quality > 0) {
             quality = quality - 1
+        }
+    }
+
+    protected open fun age() {
+        sellIn = sellIn - 1
+    }
+
+    protected open fun update3() {
+        if (sellIn < 0) {
+            if (quality > 0) {
+                quality = quality - 1
+            }
         }
     }
 }
@@ -46,6 +38,14 @@ class Brie(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, qua
     override fun update1() {
         if (quality < 50) {
             quality = quality + 1
+        }
+    }
+
+    override fun update3() {
+        if (sellIn < 0) {
+            if (quality < 50) {
+                quality = quality + 1
+            }
         }
     }
 }
@@ -68,8 +68,16 @@ class Pass(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, qua
             }
         }
     }
+
+    override fun update3() {
+        if (sellIn < 0) {
+            quality = 0
+        }
+    }
 }
 
 class Sulfuras(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
     override fun update1() {}
+    override fun age() {}
+    override fun update3() {}
 }
