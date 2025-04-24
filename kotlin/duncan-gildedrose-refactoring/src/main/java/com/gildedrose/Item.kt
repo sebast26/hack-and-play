6fc9d876bf22d830a4506b35388b72cd6f61548e
Sebastian Górecki <gorecki.sebastian@gmail.com>
2025-04-24 22:14:17 +0200

    Duncan GildedRose Refactoring: naming policies

--- kotlin/duncan-gildedrose-refactoring/src/main/java/com/gildedrose/Item.kt
+++ kotlin/duncan-gildedrose-refactoring/src/main/java/com/gildedrose/Item.kt
@@ -8,24 +8,39 @@ open class Item(
     override fun toString() = "$name, $sellIn, $quality"
 }
 
-class BaseItem(
-    name: String,
-    sellIn: Int,
-    quality: Int,
-    private val aging: () -> Int = { 1 },
-    private val degradation: (Int, Int) -> Int = { sellIn: Int, quality: Int ->
+object Aging {
+    val standard: () -> Int = { 1 }
+    val none: () -> Int = { 0 }
+}
+
+object Degradation {
+    val standard: (Int, Int) -> Int = { sellIn: Int, quality: Int ->
         when {
             sellIn < 0 -> 2
             else -> 1
         }
-    },
-    private val saturation: (Int) -> Int = { quality: Int ->
+    }
+    val none: (Int, Int) -> Int = { _, _ -> 0 }
+}
+
+object Saturation {
+    val standard: (Int) -> Int = { quality: Int ->
         when {
             quality < 0 -> 0
             quality > 50 -> 50
             else -> quality
         }
     }
+    val none: (Int) -> Int = { it }
+}
+
+class BaseItem(
+    name: String,
+    sellIn: Int,
+    quality: Int,
+    private val aging: () -> Int = Aging.standard,
+    private val degradation: (Int, Int) -> Int = Degradation.standard,
+    private val saturation: (Int) -> Int = Saturation.standard
 ) : Item(name, sellIn, quality) {
     fun update() {
         sellIn = sellIn - aging()
@@ -64,7 +79,7 @@ fun Sulfuras(name: String, sellIn: Int, quality: Int) = BaseItem(
     name,
     sellIn,
     quality,
-    aging = { 0 },
-    degradation = { _, _ -> 0 },
-    saturation = { it }
+    aging = Aging.none,
+    degradation = Degradation.none,
+    saturation = Saturation.none
 )
