dfc20ee1e5cc4ce91b932e84515bd12d78be54fb
Sebastian Górecki <gorecki.sebastian@gmail.com>
2025-04-29 21:49:00 +0200

    Fowler Refactoring: Move functions to top-level functions (not in the book).

--- kotlin/fowler-refactoring/app/src/main/kotlin/pl/sgorecki/statement.kt
+++ kotlin/fowler-refactoring/app/src/main/kotlin/pl/sgorecki/statement.kt
@@ -20,62 +20,64 @@ data class EnrichedPerformance(
     val volumeCredits: Int
 )
 
+fun playFor(performance: Performance) = plays[performance.playId]!!
+
+fun usd(number: Int) = NumberFormat.getCurrencyInstance(Locale.US).format(number / 100)
+
+fun amountFor(perf: Performance): Int {
+    var result = 0
+    when (playFor(perf).type) {
+        PlayType.TRAGEDY -> {
+            result = 40000
+            if (perf.audience > 30) {
+                result += 1000 * (perf.audience - 30)
+            }
+        }
+
+        COMEDY -> {
+            result = 30000
+            if (perf.audience > 20) {
+                result += 10000 + 500 * (perf.audience - 20)
+            }
+            result += 300 * perf.audience
+        }
+
+        else -> error("Unknown play type: ${playFor(perf).type}")
+    }
+    return result
+}
+
+fun volumeCreditsFor(performance: Performance): Int {
+    var result = 0
+    result += max(performance.audience - 30, 0)
+    if (COMEDY == playFor(performance).type) result += floor(performance.audience.toDouble() / 5).toInt()
+    return result
+}
+
+fun totalAmount(performances: List<EnrichedPerformance>): Int {
+    var result = 0
+    for (perf in performances) {
+        result += perf.amount
+    }
+    return result
+}
+
+fun totalVolumeCredits(performances: List<EnrichedPerformance>): Int {
+    var result = 0
+    for (perf in performances) {
+        result += perf.volumeCredits
+    }
+    return result
+}
+
+fun enrichPerformance(performance: Performance) = EnrichedPerformance(
+    performance = performance,
+    play = playFor(performance),
+    amount = amountFor(performance),
+    volumeCredits = volumeCreditsFor(performance),
+)
+
 fun statement(invoice: Invoice, plays: Map<String, Play>): String {
-    fun playFor(performance: Performance): Play = plays[performance.playId]!!
-
-    fun amountFor(perf: Performance): Int {
-        var result = 0
-        when (playFor(perf).type) {
-            PlayType.TRAGEDY -> {
-                result = 40000
-                if (perf.audience > 30) {
-                    result += 1000 * (perf.audience - 30)
-                }
-            }
-
-            COMEDY -> {
-                result = 30000
-                if (perf.audience > 20) {
-                    result += 10000 + 500 * (perf.audience - 20)
-                }
-                result += 300 * perf.audience
-            }
-
-            else -> error("Unknown play type: ${playFor(perf).type}")
-        }
-        return result
-    }
-
-    fun volumeCreditsFor(performance: Performance): Int {
-        var result = 0
-        result += max(performance.audience - 30, 0)
-        if (COMEDY == playFor(performance).type) result += floor(performance.audience.toDouble() / 5).toInt()
-        return result
-    }
-
-    fun totalAmount(performances: List<EnrichedPerformance>): Int {
-        var result = 0
-        for (perf in performances) {
-            result += perf.amount
-        }
-        return result
-    }
-
-    fun totalVolumeCredits(performances: List<EnrichedPerformance>): Int {
-        var result = 0
-        for (perf in performances) {
-            result += perf.volumeCredits
-        }
-        return result
-    }
-
-    fun enrichPerformance(performance: Performance) = EnrichedPerformance(
-        performance = performance,
-        play = playFor(performance),
-        amount = amountFor(performance),
-        volumeCredits = volumeCreditsFor(performance),
-    )
-
     val performances = invoice.performances.map { it -> enrichPerformance(it) }
     val statementData = StatementData(
         customer = invoice.customer,
@@ -84,14 +86,10 @@ fun statement(invoice: Invoice, plays: Map<String, Play>): String {
         totalVolumeCredits = totalVolumeCredits(performances),
     )
 
-    return renderPlainText(statementData, plays)
+    return renderPlainText(statementData)
 }
 
-fun renderPlainText(data: StatementData, plays: Map<String, Play>): String {
-    fun usd(number: Int): String {
-        return NumberFormat.getCurrencyInstance(Locale.US).format(number / 100)
-    }
-
+fun renderPlainText(data: StatementData): String {
     var result = "Statement for ${data.customer}\n"
     for (perf in data.performances) {
         result += "    ${perf.play.name}: ${usd(perf.amount)} (${perf.performance.audience} seats)\n"
