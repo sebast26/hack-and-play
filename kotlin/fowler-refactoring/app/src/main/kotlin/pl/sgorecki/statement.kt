fd8f5c1c6bef5cabfe6ba674d6aac5b7f06e40a6
Sebastian Górecki <gorecki.sebastian@gmail.com>
2025-04-29 22:04:27 +0200

    Fowler Refactoring: IMPORTANT: And now after refactoring it is easy to write an HTML version.

--- kotlin/fowler-refactoring/app/src/main/kotlin/pl/sgorecki/statement.kt
+++ kotlin/fowler-refactoring/app/src/main/kotlin/pl/sgorecki/statement.kt
@@ -27,3 +27,21 @@ fun renderPlainText(data: StatementData): String {
     result += "You earned ${data.totalVolumeCredits} credits\n"
     return result
 }
+
+fun htmlStatement(invoice: Invoice, plays: Map<String, Play>): String {
+    return renderHtml(createStatementData(invoice))
+}
+
+fun renderHtml(data: StatementData): String {
+    var result = "<h1>Statement for ${data.customer}</h1>\n"
+    result += "<table>\n"
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>\n"
+    for (perf in data.performances) {
+        result += "    <tr><td>${perf.play.name}</td><td>${perf.performance.audience}</td>"
+        result += "<td>${usd(perf.amount)}</td></tr>\n"
+    }
+    result += "</table>\n"
+    result += "<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n"
+    result += "<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n"
+    return result
+}
--- kotlin/fowler-refactoring/app/src/test/kotlin/pl/sgorecki/StatementTest.kt
+++ kotlin/fowler-refactoring/app/src/test/kotlin/pl/sgorecki/StatementTest.kt
@@ -5,8 +5,13 @@ import org.junit.jupiter.api.Test
 
 class StatementTest {
     @Test
-    fun correctStatement() {
+    fun correctPlainTextStatement() {
         assertEquals(expected, statement(invoices[0], plays))
+    }
+
+    @Test
+    fun correctHtmlStatement() {
+        assertEquals(expectedHtml, htmlStatement(invoices[0], plays))
     }
 }
 
@@ -17,5 +22,18 @@ private val expected = """
         Othello: $500.00 (40 seats)
     Amount owed is $1,730.00
     You earned 47 credits
+    
+""".trimIndent()
+
+private val expectedHtml = """
+    <h1>Statement for BigCo</h1>
+    <table>
+    <tr><th>play</th><th>seats</th><th>cost</th></tr>
+        <tr><td>Hamlet</td><td>55</td><td>${'$'}650.00</td></tr>
+        <tr><td>As You Like It</td><td>35</td><td>${'$'}580.00</td></tr>
+        <tr><td>Othello</td><td>40</td><td>${'$'}500.00</td></tr>
+    </table>
+    <p>Amount owed is <em>${'$'}1,730.00</em></p>
+    <p>You earned <em>47</em> credits</p>
     
 """.trimIndent()
