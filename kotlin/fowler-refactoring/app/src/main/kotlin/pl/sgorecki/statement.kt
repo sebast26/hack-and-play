62fcc9a45e0749004412ea3717b86d0fdff1a0dc
Sebastian Górecki <gorecki.sebastian@gmail.com>
2025-04-29 21:00:20 +0200

    Fowler Refactoring: Adhere to convention in naming return variables.

--- kotlin/fowler-refactoring/app/src/main/kotlin/pl/sgorecki/statement.kt
+++ kotlin/fowler-refactoring/app/src/main/kotlin/pl/sgorecki/statement.kt
@@ -44,19 +44,19 @@ fun statement(invoice: Invoice, plays: Map<String, Play>): String {
     }
 
     fun totalVolumeCredits(): Int {
-        var volumeCredits = 0
+        var result = 0
         for (perf in invoice.performances) {
-            volumeCredits += volumeCreditsFor(perf)
+            result += volumeCreditsFor(perf)
         }
-        return volumeCredits
+        return result
     }
 
     fun totalAmount(): Int {
-        var totalAmount = 0
+        var result = 0
         for (perf in invoice.performances) {
-            totalAmount += amountFor(perf)
+            result += amountFor(perf)
         }
-        return totalAmount
+        return result
     }
 
     var result = "Statement for ${invoice.customer}\n"
