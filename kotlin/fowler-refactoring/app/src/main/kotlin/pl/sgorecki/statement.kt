b51f9c79623592761111e8cdb75f2b3cc68822b7
Sebastian Górecki <gorecki.sebastian@gmail.com>
2025-04-29 21:32:58 +0200

    Fowler Refactoring: Play name should come from intermediary data.

--- kotlin/fowler-refactoring/app/src/main/kotlin/pl/sgorecki/statement.kt
+++ kotlin/fowler-refactoring/app/src/main/kotlin/pl/sgorecki/statement.kt
@@ -8,47 +8,54 @@ import kotlin.math.max
 
 data class StatementData(
     val customer: String,
-    val performances: List<Performance>
+    val performances: List<EnrichedPerformance>
 )
 
+data class EnrichedPerformance(val performance: Performance, val play: Play)
+
 fun statement(invoice: Invoice, plays: Map<String, Play>): String {
+    fun playFor(performance: Performance): Play = plays[performance.playId]!!
+
+    fun enrichPerformance(performance: Performance) = EnrichedPerformance(performance, playFor(performance))
+
     val statementData = StatementData(
         customer = invoice.customer,
-        performances = invoice.performances
+        performances = invoice.performances.map { it -> enrichPerformance(it) }
     )
+
     return renderPlainText(statementData, plays)
 }
 
 fun renderPlainText(data: StatementData, plays: Map<String, Play>): String {
     fun playFor(performance: Performance): Play = plays[performance.playId]!!
 
-    fun amountFor(perf: Performance): Int {
+    fun amountFor(perf: EnrichedPerformance): Int {
         var result = 0
-        when (playFor(perf).type) {
+        when (playFor(perf.performance).type) {
             PlayType.TRAGEDY -> {
                 result = 40000
-                if (perf.audience > 30) {
-                    result += 1000 * (perf.audience - 30)
+                if (perf.performance.audience > 30) {
+                    result += 1000 * (perf.performance.audience - 30)
                 }
             }
 
             COMEDY -> {
                 result = 30000
-                if (perf.audience > 20) {
-                    result += 10000 + 500 * (perf.audience - 20)
+                if (perf.performance.audience > 20) {
+                    result += 10000 + 500 * (perf.performance.audience - 20)
                 }
-                result += 300 * perf.audience
+                result += 300 * perf.performance.audience
             }
 
-            else -> error("Unknown play type: ${playFor(perf).type}")
+            else -> error("Unknown play type: ${playFor(perf.performance).type}")
         }
         return result
     }
 
-    fun volumeCreditsFor(performance: Performance): Int {
+    fun volumeCreditsFor(performance: EnrichedPerformance): Int {
         var result = 0
-        result += max(performance.audience - 30, 0)
-        if (COMEDY == playFor(performance).type) result += floor(performance.audience.toDouble() / 5).toInt()
+        result += max(performance.performance.audience - 30, 0)
+        if (COMEDY == playFor(performance.performance).type) result += floor(performance.performance.audience.toDouble() / 5).toInt()
         return result
     }
 
@@ -74,7 +81,7 @@ fun renderPlainText(data: StatementData, plays: Map<String, Play>): String {
 
     var result = "Statement for ${data.customer}\n"
     for (perf in data.performances) {
-        result += "    ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n"
+        result += "    ${perf.play.name}: ${usd(amountFor(perf))} (${perf.performance.audience} seats)\n"
     }
     result += "Amount owed is ${usd(totalAmount())}\n"
     result += "You earned ${totalVolumeCredits()} credits\n"
