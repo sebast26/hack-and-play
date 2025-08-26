b0c02d7815f333bb4633933fd6d2f778a5b188f9
Sebastian Górecki <gorecki.sebastian@gmail.com>
2025-08-26 21:36:09 +0200

    Flutter: daily_widgets: SearchBar and SearchAnchor test

--- flutter/daily_widgets/lib/main.dart
+++ flutter/daily_widgets/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:daily_widgets/widgets/cupertino_switch.dart';
+import 'package:daily_widgets/widgets/searchbar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const CupertinoSwitchTest(),
+      home: const SearchBarTest(),
     );
   }
 }
---
+++ flutter/daily_widgets/lib/widgets/searchbar.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class SearchBarTest extends StatefulWidget {
+  const SearchBarTest({super.key});
+
+  @override
+  State<SearchBarTest> createState() => _SearchBarTestState();
+}
+
+class _SearchBarTestState extends State<SearchBarTest> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('SearchBar & SearchAnchor')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Seba'),
+            SearchBar(
+              leading: const Icon(Icons.search),
+              hintText: 'Search',
+              backgroundColor: WidgetStateProperty.all(Colors.blue),
+              shadowColor: WidgetStateProperty.all(Colors.black),
+              elevation: WidgetStateProperty.all(4.0),
+              shape: WidgetStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)
+                )
+              ),
+              padding: WidgetStateProperty.all(
+                EdgeInsets.symmetric(horizontal: 16.0)
+              ),
+              onSubmitted: (value) {
+                print('Search sumbitted: $value');
+              },
+            ),
+            const Text('Some space'),
+            SearchAnchor.bar(
+              barLeading: Icon(Icons.search),
+              barHintText: 'Search',
+              barBackgroundColor: WidgetStateProperty.all(Colors.blue),
+              barElevation: WidgetStateProperty.all(4.0),
+              barShape: WidgetStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)
+                )
+              ),
+              barPadding: WidgetStateProperty.all(
+                EdgeInsets.symmetric(horizontal: 16.0)
+              ),
+              suggestionsBuilder: (BuildContext context, SearchController controller) {
+                final String input = controller.value.text;
+                return [
+                  ListTile(
+                    title: Text('Something $input'),
+                    onTap: () {
+                      print('You have just selected Something $input');
+                      controller.closeView('Something $input');
+                    }
+                  ),
+                  ListTile(
+                    title: Text('Other $input'),
+                    onTap: () {
+                      print('You have just selected Other $input');
+                      controller.closeView('Other $input');
+                    },
+                  )
+                ];
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
