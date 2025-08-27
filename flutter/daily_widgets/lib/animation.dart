4f464ac9ea2d7edfedb8f1d58d6e9ba9e7408926
Sebastian Górecki <gorecki.sebastian@gmail.com>
2025-08-27 19:44:41 +0200

    Flutter: daily_widgets: monitoring progress of the animation

--- flutter/daily_widgets/lib/animation.dart
+++ flutter/daily_widgets/lib/animation.dart
@@ -36,7 +36,15 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
       vsync: this,
       duration: const Duration(seconds: 2),
     );
-    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+    ..addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
+    })
+    ..addStatusListener((status) => print('$status'));
     controller.forward();
   }
 
