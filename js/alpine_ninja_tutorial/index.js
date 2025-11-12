3952d90335b687958c53e2d1cbf29fa46bfb09a9
Sebastian Górecki <gorecki.sebastian@gmail.com>
2025-11-12 22:05:47 +0100

    JS: Alpine.js crash course - models & forms

--- js/alpine_ninja_tutorial/index.js
+++ js/alpine_ninja_tutorial/index.js
@@ -1,10 +1,10 @@
 document.addEventListener('alpine:init', () => {
-    Alpine.data('counter', () => ({
-        count: 0,
-        name: 'Mario',
-
-        logCount() {
-            console.log(this.count)
-        },
+    Alpine.data('signupForm', () => ({
+        username: '',
+        password: '',
+        passwordConfirm: '',
+        belt: '',
+        bio: '',
+        newsletter: true,
     }))
 })
