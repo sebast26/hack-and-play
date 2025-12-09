30ff9d526d96e1992b4521942d332856b9ae7070
Sebastian Górecki <gorecki.sebastian@gmail.com>
2025-12-09 11:58:57 +0100

    kotlin: playing with uchuhimo-konf: sample example

--- kotlin/github-konf/app/src/main/kotlin/pl/sgorecki/App.kt
+++ kotlin/github-konf/app/src/main/kotlin/pl/sgorecki/App.kt
@@ -1,6 +1,8 @@
 package pl.sgorecki
 
+import com.uchuhimo.konf.Config
 import com.uchuhimo.konf.ConfigSpec
+import com.uchuhimo.konf.source.yaml
 
 object ServerSpec : ConfigSpec() {
     val host by optional("0.0.0.0")
@@ -8,5 +10,15 @@ object ServerSpec : ConfigSpec() {
 }
 
 fun main() {
-    println(ServerSpec.prefix)
+
+    val config = Config {
+        addSpec(ServerSpec)
+    }.from.yaml.string("""
+       server:
+         host: 0.0.0.0
+         tcp_port: 8080
+    """.trimIndent())
+
+    println("Server ${config[ServerSpec.host]} running on port ${config[ServerSpec.tcpPort]}")
+
 }
